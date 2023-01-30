5cd88340fe3d02f67954564a45300ad4048b0a25
Aldhanekaa <aldhanekadev@gmail.com>
2023-01-30 21:22:38 +0700

    add: missions section

--- components/home/mission.tsx
+++ components/home/mission.tsx
@@ -5,7 +5,7 @@ export default function MissionSection() {
   return (
     <div className="w-full mt-40 relative mb-40">
       <div
-        className="w-full absolute bottom-0 h-2/6"
+        className="w-full absolute bottom-0 h-2/6 lg:block hidden"
         style={{
           background: "#2C2C2C",
           border: "1px solid #707070",
@@ -20,26 +20,18 @@ export default function MissionSection() {
             src="/quotationMark.png"
             className="absolute w-50 h-50 left-12 -top-10"
           />
-          <p className="text-5xl text-gray-400 text-center align-center mt-10 leading-normal">
+          <p className="text-2xl md:text-5xl text-gray-400 text-center align-center mt-10 leading-normal">
             “To inspire more students and make them more interested in the STEAM
             field and business, and to encourage high school students to develop
             knowledge on science and technology”
           </p>
         </div>
         <div className="w-full grid grid-cols-11">
-          <div className="col-span-1"></div>
-          {/* <div
-            id="missionImage"
-            className="col-span-9 bg-contain bg-center bg-no-repeat"
-            style={{
-              height: "670px",
-              maxHeight: "670px",
-              background: "url(/img/missionImage.png)",
-            }}
-          ></div> */}
+          <div className="col-span-1 lg:block hidden"></div>
+
           <div
             id="missionImage"
-            className="col-span-9 overflow-hidden relative mt-20"
+            className="col-span-9 overflow-hidden relative mt-20 lg:block hidden"
             style={{
               height: "670px",
               maxHeight: "670px",
@@ -52,7 +44,7 @@ export default function MissionSection() {
               className="w-full absolute top-0 bottom-0 my-auto"
             />
           </div>
-          <div className="col-span-1"></div>
+          <div className="col-span-1 lg:block hidden"></div>
         </div>
       </div>
     </div>
